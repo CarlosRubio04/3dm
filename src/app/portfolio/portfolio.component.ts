a1ade9b629f153222c7e628daaaf7ad97dd3bbb4
Carlos Rubio <crubio@acsendo.com>
2019-05-23 07:44:27 -0500

    Portafolio con data

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -16,31 +16,9 @@ export class PortfolioComponent implements OnInit {
 
     items: any = [];
     modal: boolean = false;
-    modalData: any = {
-        action: "www.twitter.com",
-        description: "Hola esto es otra prueba",
-        id: 1539354464599,
-        title: "Prueba 3",
-        claro: {
-            valor: "El costo del servicio es de $718 IVA incluido diario.",
-            activar: "Enviando un mensaje de texto al código 37787 con la palabra SOLICITO HAPPY.",
-            cancelar: "Ingresando aquí podrá consultar las suscripciones activas y cancelarlas, o enviando un mensaje de texto al código 37787 con la palabra HAPPY SALIR."
-        },
-        tigo: {
-            valor: "2El costo del servicio es de $718 IVA incluido diario.",
-            activar: "Enviando un mensaje de texto al código 37787 con la palabra SOLICITO HAPPY.",
-            cancelar: "Ingresando aquí podrá consultar las suscripciones activas y cancelarlas, o enviando un mensaje de texto al código 37787 con la palabra HAPPY SALIR."
-        },
-        movistar: {
-            valor: "3El costo del servicio es de $718 IVA incluido diario.",
-            activar: "Enviando un mensaje de texto al código 37787 con la palabra SOLICITO HAPPY.",
-            cancelar: "Ingresando aquí podrá consultar las suscripciones activas y cancelarlas, o enviando un mensaje de texto al código 37787 con la palabra HAPPY SALIR."
-        }
-    };
+    modalData: any = {};
 
     operadorActive: any = {};
-
-    
 
     constructor(private mainService: MainService) { }
 
